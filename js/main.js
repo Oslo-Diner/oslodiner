7e933a1072d7ec4fa13162fd8137bc94ddf4e678
Ogundare Samuel <30951791+orsdev@users.noreply.github.com>
2019-09-03 06:59:11 +0100

    fixed home button redirect error

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
-function Redirect() {
-    window.location.assign("./menu/menu.html");
-}
+
+
+//add and listen event
+function eventListener(){
+    //get element from dom
+   const menu = document.getElementById('menu');
+    
+    //add click event
+    menu.addEventListener('click' , redirect);
+ };
+
+function redirect(e){
+    //get clicked element from dom
+    let target = e.target;
+   
+    //redirect to menu page if true
+    if(target.tagName = 'BUTTON'){
+        window.location.assign('./menu/menu.html');
+     }   
+};
+
+//call function
+eventListener();
